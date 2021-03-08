c759701e2cb0d10031310f239c2d9979484a9c22
Paola Bassi <pbassi@monoceros-lnx.it.esteco.net>
2021-03-08 09:03:21 +0100

    Initial commit

---
+++ src/main/java/dssc/hello/Main.java
@@ -0,0 +1,14 @@
+package dssc.hello;
+
+public class Main {
+
+    public static void main(String... args) {
+        if(args.length>0){
+            System.out.println("Hello "+ args[0] + "!");
+        } else {
+            System.out.println("Hello stranger!");
+        }
+
+    }
+}
+
